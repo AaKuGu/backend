98653f5b89e6fb9812a9b9044e8b88a1ebc6d772
AaKuGu <prathamtogupta11@gmail.com>
2023-02-12 15:52:36 +0530

    added /register route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { routes } from "./router/router.js";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT;
 
--- router/router.js
+++ router/router.js
@@ -2,5 +2,14 @@ import { Router } from "express";
 export const routes = Router();
 
 routes.get("/serverinfo", (req, res) => {
-  res.status(200).json({ message: "/api/get working fine" });
+  res.status(200).json({ message: "/api/serverinfo working fine" });
+});
+
+routes.post("/register", (req, res) => {
+  try {
+    const { email } = req.body;
+    res.status(200).send({ message: email });
+  } catch (error) {
+    res.status(404).send({ error: error.message });
+  }
 });
