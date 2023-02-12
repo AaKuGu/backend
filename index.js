435f67401a08d22b6da7faac1d355c2a3ee78288
AaKuGu <prathamtogupta11@gmail.com>
2023-02-12 15:23:33 +0530

    a lot

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use("/api", routes);
+
 app.get("/", (req,res) => {
     res.status(200).send("/ is working!")
 })
 
-app.use("/api",routes);
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
