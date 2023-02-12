d317cba5cee8d471a7aea149b19ec9464c707957
AaKuGu <prathamtogupta11@gmail.com>
2023-02-12 15:14:10 +0530

    app.use to app.get

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use("/api",routes);
+app.get("/api",routes);
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
