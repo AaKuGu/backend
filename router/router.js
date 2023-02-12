c45ce5908a37a859715d6d3222d943c091d14424
AaKuGu <prathamtogupta11@gmail.com>
2023-02-12 16:00:21 +0530

    email and pass in .env

--- router/router.js
+++ router/router.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 export const routes = Router();
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 routes.get("/serverinfo", (req, res) => {
   res.status(200).json({ message: "/api/serverinfo working fine" });
@@ -9,6 +13,38 @@ routes.post("/register", (req, res) => {
   try {
     const { email } = req.body;
     res.status(200).send({ message: email });
+
+    // starting nodeMailer code
+
+    console.log(
+      "emial",
+      process.env.EMAIL,
+      "password : ",
+      process.env.PASSWORD
+    );
+
+    // let mailTransporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.EMAIL,
+    //     pass: process.env.PASSWORD,
+    //   },
+    // });
+
+    // let mailDetails = {
+    //   from: "xyz@gmail.com",
+    //   to: "abc@gmail.com",
+    //   subject: "Test mail",
+    //   text: "Node.js testing mail for GeeksforGeeks",
+    // };
+
+    // mailTransporter.sendMail(mailDetails, function (err, data) {
+    //   if (err) {
+    //     console.log("Error Occurs");
+    //   } else {
+    //     console.log("Email sent successfully");
+    //   }
+    // });
   } catch (error) {
     res.status(404).send({ error: error.message });
   }
